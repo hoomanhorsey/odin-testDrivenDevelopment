15dd1d0c391d35844b59e765f95bd0a518eac87a
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2024-11-12 14:42:56 +1100

    Sync

---
+++ testFunctions.js
@@ -0,0 +1,95 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// old exports language
+// module.exports = sum;
+
+// A capitalize function that takes a string and returns it with the first character capitalized.
+function capitalize(string) {
+  let firstChar = string[0];
+  let upperFirstChar = firstChar.toUpperCase();
+
+  return string.replace(firstChar, upperFirstChar);
+}
+
+// A reverseString function that takes a string and returns it reversed.
+function reverse(string) {
+  return string.split("").reverse().join("");
+}
+
+// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
+
+// A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
+// Don’t forget to test wrapping from z to a. For example, caesarCipher('xyz', 3) should return 'abc'.
+// Don’t forget to test case preservation. The shifted lettercase should follow the original lettercase.
+//  For example, caesarCipher('HeLLo', 3) should return 'KhOOr'.
+// Don’t forget to test punctuation. Punctuations, spaces, and other non-alphabetical characters should remain unchanged.
+//  For example, caesarCipher('Hello, World!', 3) should return 'Khoor, Zruog!'.
+// For this one, you may want to split the final function into a few smaller functions.
+// One concept of Testing is that you don’t need to explicitly test every function you write… Just the public ones.
+// So in this case you only need tests for the final caesarCipher function. If it works as expected you can rest assured
+// that your smaller helper functions are doing what they’re supposed to.
+
+function caesarCipher(string, key) {
+  // turn string to an array
+  // loop through each letter of the array
+  // apply key to the letter
+  // insert resulting letter into new array
+
+  const array = string.split("");
+
+  console.log(array);
+
+  const shiftedArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+
+    let pattern = /[a-zA-Z]/;
+
+    if (pattern.test(array[i])) {
+      console.log("is true");
+
+      shiftedArray.push(String.fromCharCode(key + array[i].charCodeAt(0)));
+    } else {
+      shiftedArray.push(array[i]);
+    }
+
+    // apply key function(char, key)
+    //    if (char === punctuation, spaces or non-alpha,) then char = char.
+    //    else
+  }
+  let cipher = shiftedArray.join("");
+  console.log(shiftedArray);
+
+  return cipher;
+}
+
+export { sum, capitalize, reverse, calculator, caesarCipher };
+
+// An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
+
+// const object = analyzeArray([1,8,3,4,2,6]);
+
+// object == {
+//    average: 4,
+//    min: 1,
+//    max: 8,
+//    length: 6
+// };
---
+++ testFunctions.test.js
@@ -0,0 +1,62 @@
+// insert tests
+
+// old import text
+// const sum = require("./testFunctions");
+
+import {
+  sum,
+  capitalize,
+  reverse,
+  calculator,
+  caesarCipher,
+} from "./testFunctions";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+// Capitalize
+test("'lowercasestring' is returned as 'Lowercasestring'", () => {
+  expect(capitalize("lowercasestring")).toMatch(/^[A-Z]/);
+});
+
+test("Input strings and output strings match", () => {
+  expect(capitalize("lowercasestring").slice(1)).toEqual(
+    "lowercasestring".slice(1)
+  );
+});
+
+// Reverse
+
+test("'string' is returned as 'gnirts'", () => {
+  expect(reverse("string")).toBe("gnirts");
+});
+
+// calculator
+
+test("calculator, adds 20 + 5 to equal 25 ", () => {
+  expect(calculator.add(20, 5)).toBe(25);
+});
+
+test("calculator, subtracts 20 -5 to equal 15", () => {
+  expect(calculator.subtract(20, 5)).toBe(15);
+});
+test("calculator, divides 20 by 5 to equal 4", () => {
+  expect(calculator.divide(20, 5)).toBe(4);
+});
+test("calculator, multiplies 20 by 5 to equal 100 ", () => {
+  expect(calculator.multiply(20, 5)).toBe(100);
+});
+
+// caesarCipher
+test("caesarCipher, 'string' apply shift factor of 1 becomes 'tusjoh'", () => {
+  expect(caesarCipher("string", 1)).toBe("tusjoh");
+});
+
+test("caesarCipher, '9string' apply shift factor of 1 becomes '9tusjoh'", () => {
+  expect(caesarCipher("9string", 1)).toBe("9tusjoh");
+});
+
+test("caesarCipher, '9str !ing' apply shift factor of 1 becomes '9tus !joh'", () => {
+  expect(caesarCipher("9st !ring", 10)).toBe("9tu !sjoh");
+});
